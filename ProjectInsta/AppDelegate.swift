d5bd76c3a6c1ed89565413a80ab46e88f17f1692
Andy Palka <andrewmpalka@gmail.com>
2016-02-09 17:36:21 -0600

    bridging headers and app delegate updated

--- ProjectInsta/AppDelegate.swift
+++ ProjectInsta/AppDelegate.swift
@@ -9,6 +9,8 @@
 import UIKit
 import CoreData
 import Firebase
+import AWSCore
+
 
 
 @UIApplicationMain
@@ -30,6 +32,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let credentialsProvider = AWSCognitoCredentialsProvider(
+            regionType: AWSRegionType.USEast1, identityPoolId: cognitoIdentityPoolId)
+        
+        let defaultServiceConfiguration = AWSServiceConfiguration(
+            region: AWSRegionType.USEast1, credentialsProvider: credentialsProvider)
+        
+        AWSServiceManager.defaultServiceManager().defaultServiceConfiguration = defaultServiceConfiguration
+
         return true
     }
 
