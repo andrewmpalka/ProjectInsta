c1a99eb627130209e41ed2e2ba0e008cdfddc8df
Michael Sandoval <michaelsandoval@Michaels-MacBook-Pro-2.local>
2016-02-09 12:40:12 -0600

    poppulated the cell in tableview with posts

--- ProjectInsta/HomeViewController.swift
+++ ProjectInsta/HomeViewController.swift
@@ -7,18 +7,50 @@
 //
 
 import UIKit
-
-class HomeViewController: UIViewController {
-
+import Firebase
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
 
+        
         DataService.ds.REF_POST.observeEventType(.Value, withBlock: { snapshot in
-          
-            print(snapshot.value)
+            self.posts = []
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                
+                for snap in snapshots {
+//                    print("SNAP \(snap)")
+
+                    if let postDictionary = snap.value as? (Dictionary<String, AnyObject>) {
+                        let key = snap.key
+                        let post = Post(postKey: key, dictionary: postDictionary)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+        
         })
+        
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = (tableView.dequeueReusableCellWithIdentifier("CellID"))
+        let post = posts[indexPath.row]
+        cell?.textLabel?.text = post.username
+        cell?.detailTextLabel?.text = post.postDescription
+        print(post.postDescription)
+        return cell!
+    }
+
 
     // MARK: - Navigation
 
