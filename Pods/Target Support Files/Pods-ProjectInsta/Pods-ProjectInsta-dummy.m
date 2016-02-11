1c03eae48c82ccf3e54051896e9149f264063b8c
richardgmartin <richardgeorgemartin@gmail.com>
2016-02-11 09:51:31 -0600

    richard commits to pods

---
+++ Pods/Target Support Files/Pods-ProjectInsta/Pods-ProjectInsta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjectInsta : NSObject
+@end
+@implementation PodsDummy_Pods_ProjectInsta
+@end
---
+++ Pods/Target Support Files/Pods-ProjectInsta/Pods-ProjectInsta-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ProjectInstaVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ProjectInstaVersionString[];
+
